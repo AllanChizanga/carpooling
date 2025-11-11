d3cb2e1f34a60b1185e404f18c4dac6b59e3fcbc
Allan Chizanga <allanofchrist@gmail.com>
2025-11-12 01:25:14 +0200

    basics

--- tests/Feature/CreateDriverRideTest.php
+++ tests/Feature/CreateDriverRideTest.php
@@ -45,7 +45,7 @@ public function test_create_driver_ride_returns_status_code_200()
         'Content-Type' => 'application/json'
     ])->postJson('/api/carpooling/drivers/create-driver-ride', $rideData);
 
-    $response->assertStatus(200);
+    $response->assertStatus(201);
 }
 
 }
